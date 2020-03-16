3a3f8497e075782086e431a664c08eeba7abc654
hoangviethung <hoangviet.hung.009@gmail.com>
2020-03-16 17:59:57 +0700

    DONE TRANG GIỚI THIỆU

--- src/js/main.js
+++ src/js/main.js
@@ -105,6 +105,52 @@ const homeSlider = () => {
 	})
 }
 
+const normalSlider = () => {
+	var swiper = new Swiper('.slider-normal-banner .swiper-container', {
+		loop: true,
+		speed: 1000,
+		autoplay: {
+			delay: 3000,
+			disableOnInteraction: false,
+		},
+		grabCursor: true,
+		watchSlidesProgress: true,
+		mousewheelControl: true,
+		keyboardControl: true,
+		pagination: {
+			el: '.slider-normal-banner .swiper-pagination',
+			type: 'bullets',
+			clickable: true,
+		},
+		on: {
+			progress: function() {
+				var swiper = this;
+				for (var i = 0; i < swiper.slides.length; i++) {
+					var slideProgress = swiper.slides[i].progress;
+					var innerOffset = swiper.width * 0.5;
+					var innerTranslate = slideProgress * innerOffset;
+					swiper.slides[i].querySelector(".swiper-inner").style.transform =
+						"translate3d(" + innerTranslate + "px, 0, 0)";
+				}
+			},
+			touchStart: function() {
+				var swiper = this;
+				for (var i = 0; i < swiper.slides.length; i++) {
+					swiper.slides[i].style.transition = "";
+				}
+			},
+			setTransition: function(speed) {
+				var swiper = this;
+				for (var i = 0; i < swiper.slides.length; i++) {
+					swiper.slides[i].style.transition = speed + "ms";
+					swiper.slides[i].querySelector(".swiper-inner").style.transition =
+						speed + "ms";
+				}
+			}
+		}
+	})
+}
+
 // SLIDER NHÀ PHÂN PHỐI !!!
 const distributorSlider = () => {
 	var swiper = new Swiper('.slider-distributor .swiper-container', {
@@ -171,14 +217,29 @@ const customerSlider = () => {
 	})
 }
 
+const setHeightOverFolowBySomeElement = (selector) => {
+	$(selector).each(function() {
+		const heightGet = $(this).find('[data-getHeight]').innerHeight();
+		const heightSet = $(this).find('[data-setHeight]');
+		const responsive = heightSet.attr('data-setHeight');
+		if (window.innerWidth > responsive) {
+			heightSet.css('max-height', heightGet)
+		}
+	})
+}
 
 document.addEventListener('DOMContentLoaded', () => {
 	// LOADING !!!
-	Loading();
+	Loading().then(() => {
+		new WOW().init();
+		// GET HEIGHT SOMWE ELEMENT
+		setHeightOverFolowBySomeElement('.about-1,.about-3');
+	});
 	// HEADER !!!
 	activeHeaderWhenScroll();
 	// SLIDER HOME !!!
 	homeSlider();
+	normalSlider()
 	// SLIDER NHÀ PHÂN PHỐI !!!
 	distributorSlider();
 	// SLIDER KHÁCH HÀNG !!!
@@ -188,5 +249,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 document.addEventListener('resize', () => {
 	// TAM GIÁC LOGO HEADER !!!
-	activeHeaderWhenScroll()
+	activeHeaderWhenScroll();
+	setHeightOverFolowBySomeElement('.about-1,.about-3');
 })
